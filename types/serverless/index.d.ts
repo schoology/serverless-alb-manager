d7374b17ffaadb27e080fb2e7d7fcba3724cf39a
Tim Trinidad <tim@schoology.com>
2019-09-14 04:18:02 -0700

    add tests

--- types/serverless/index.d.ts
+++ types/serverless/index.d.ts
@@ -1,15 +1,65 @@
 // serverless type declaration fixes - to be removed once
 // https://github.com/DefinitelyTyped/DefinitelyTyped/pull/38315 is merged and released
 
-// tslint:disable:no-any
+// tslint:disable:no-any max-classes-per-file
 
 declare module 'serverless/classes/Plugin' {
-  interface Plugin {
+  const serverlessOriginal = require('serverless');
+  declare interface Plugin {
     hooks: {
       [event: string]: () => Promise<any>;
     };
   }
+
+  declare var Plugin: {
+    prototype: Plugin;
+    new (serverless: Serverless, options: Serverless.Options): Plugin;
+  };
   export = Plugin;
+}
+
+declare module 'serverless/classes/Service' {
+  declare class Service {
+    public custom: Service.Custom;
+
+    public provider: {
+      compiledCloudFormationTemplate: {
+        Resources: {
+          [key: string]: any;
+        };
+        Outputs?: {
+          [key: string]: any;
+        };
+      };
+
+      name: string;
+      stage: string;
+      region: string;
+      runtime?: string;
+      timeout?: number;
+      versionFunctions: boolean;
+
+      naming: Record<string, () => string>;
+    };
+    constructor(serverless: Serverless, data: {});
+
+    public load(rawOptions: {}): Promise<any>;
+    public setFunctionNames(rawOptions: {}): void;
+
+    public getServiceName(): string;
+    public getAllFunctions(): string[];
+    public getAllFunctionsNames(): string[];
+    public getFunction(functionName: string): Serverless.FunctionDefinition;
+    public getEventInFunction(eventName: string, functionName: string): Serverless.Event;
+    public getAllEventsInFunction(functionName: string): Serverless.Event[];
+
+    public mergeResourceArrays(): void;
+    public validate(): Service;
+
+    public update(data: {}): {};
+  }
+
+  export = Service;
 }
 
 declare module 'serverless/plugins/aws/provider/awsProvider' {
@@ -36,7 +86,6 @@ declare module 'serverless' {
     }
   }
 
-  // tslint:disable-next-line
   declare class Serverless extends serverlessOriginal {}
 
   export = Serverless;
